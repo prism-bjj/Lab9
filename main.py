dd405179a7d4d28da2633713beba2c825dbf7f64
taylorschmitt <tayloreschmitt@gmail.com>
2024-04-11 15:37:42 -0400

    I made changes to the encode function so that it wouldn't print anything, only return. I also changed choice 2 and added the decode function.

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 def encode(strinput):
-    for i in range (0, len(strinput)):
-        print(((int(strinput[i]) + 3) % 10), end='') 
+    encoded_pass = []
+    for i in strinput:
+        new_pass = (int(i) + 3) % 10
+        encoded_pass.append(str(new_pass))
+    return ''.join(encoded_pass)
+
+def decode(code):
+    decoded_pass = []
+    for i in code:
+        new_pass = (int(i) - 3) % 10
+        decoded_pass.append(str(new_pass))
+    return ''.join(decoded_pass)
 
 def menu():
-    print("""Menu1. Encode
-          2. Decode
-          3. Exit""")
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
     
 if __name__ == "__main__":
-    password = []
     menu()
-    choice = input("Please enter an option")
+    choice = int(input("Please enter an option: "))
     while True:
         if choice == 1:
             strinput = input("Please enter your password to encode: ")
-            password.append(encode(strinput))
-            print("Your password has been encoded and stored!")
+            encoded = encode(strinput)
+            print(encode(strinput))
+            print("\nYour password has been encoded and stored!\n")
+            menu()
+            choice = int(input("Please enter an option: "))
+
         if choice == 2:
-            if len(password) < 1:
-                print("No password yet:")
-            else:
-                print(f"The encoded password is {password.pop}")
+            print(f"The encoded password is {encoded}, and the original password is {decode(encoded)}.")
+            menu()
+            choice = int(input("Please enter an option: "))
+
         if choice == 3:
             break
